a548b1ffe886f43d0106093a3646c7971c4ca01b
alitokii <13807422+alitokii@users.noreply.github.com>
2024-07-21 16:02:46 -0700

    Fixing time

--- htdocs/js/schedule.js
+++ htdocs/js/schedule.js
@@ -7,7 +7,6 @@ document.addEventListener('DOMContentLoaded', function() {
     // Load events from localStorage
     loadEvents();
 
-<<<<<<< HEAD
     addEventBtn.addEventListener('click', () => {
       document.getElementById('eventPopup').style.display = 'block';
     });
@@ -38,17 +37,8 @@ document.addEventListener('DOMContentLoaded', function() {
       eventPopup.style.display = 'none';
     });
 
-    // // This will listen for the click event on the "Add Event" button
-    // schedule.addEventListener('click', (e) => {
-    //   if (e.target.classList.contains('add-event-btn')) {
-    //     const day = e.target.dataset.day;
-    //     showEventPopup(day);
-    //   }
-    // });
-=======
     // Highlight the current day
     highlightCurrentDay();
->>>>>>> 0137a292965b4c365d4f540cce3751059f64dc91
 
     eventForm.addEventListener('submit', function(e) {
         e.preventDefault();
